d32489c4683498ba164775678a8d8873f634a90a
b00l <wwgeek@163.com>
2024-05-18 15:58:39 +0800

    feat: init

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,5 @@
+function MainLayout({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>;
+}
+
+export default MainLayout;
---
+++ app/(main)/lookup/page.tsx
@@ -0,0 +1,5 @@
+function Lookup() {
+  return <>Lookup</>;
+}
+
+export default Lookup;

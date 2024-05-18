484312f92be3a718078c514a487599bbfcd4c2fc
Django <djangoomo@gmail.com>
2024-05-18 17:07:39 +0800

    chore: abi

--- config/chain.tsx
+++ config/chain.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { mainnet, sepolia, scroll, scrollSepolia } from 'wagmi/chains'
 
 // Get projectId at https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,20 +9,20 @@ export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 if (!projectId) throw new Error('Project ID is not defined')
 
 const metadata = {
-  name: 'GitID',
-  description: 'GitID Example',
-  url: 'http://git.id/', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
+    name: 'GitID',
+    description: 'GitID Example',
+    url: 'http://git.id/', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const
+const chains = [scroll, scrollSepolia] as const
 export const config = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-  ssr: true,
-  storage: createStorage({
-    storage: cookieStorage
-  }),
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage
+    }),
 })

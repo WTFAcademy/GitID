3ef74ef60fbcff04ea0df473d574856c9d9e719b
chongqiangchen <chongqiangchen@gmail.com>
2024-05-19 12:39:40 +0800

    feat: update mint

--- components/mint-card/mint-section.tsx
+++ components/mint-card/mint-section.tsx
@@ -1,5 +1,8 @@
 import {Button} from "@/components/ui/button";
 import {useMint} from "@/lib/hooks/use-mint";
+import {useAtomValue} from "jotai/index";
+import {signInfoAtom} from "@/lib/store/mint";
+import {truncate} from "@/lib/truncate";
 import Link from "next/link";
 
 const MintSection = () => {
@@ -7,14 +10,39 @@ const MintSection = () => {
         mint,
         isLoading,
         isError,
-        error
+        error,
+        hash,
+        isSuccess
     } = useMint();
+    const signInfo = useAtomValue<any>(signInfoAtom);
+
+    if (isSuccess) {
+        return (
+            <div className="w-full flex flex-col items-center">
+                <h2 className="text-[32px] font-bold mb-6">Minted</h2>
+                <div className="flex flex-col items-center space-y-[10px] mb-[56px]">
+                    <span className="text-gray-400 text-xs font-bold">Transaction Hash</span>
+                    <Link
+                        href={`https://etherscan.io/tx/${hash}`}
+                        className="py-2 px-6 rounded-lg bg-white shadow text-sm w-[200px] text-center"
+                    >{truncate(hash!, 10)}</Link>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <>
             <h3 className="text-[18px] font-bold mb-10">Mint Git.ID</h3>
             <div className="w-full flex flex-col items-center">
-                <h2 className="text-[32px] font-bold mb-6">tank.git</h2>
+                <h2 className="text-[32px] font-bold mb-6">
+                    {signInfo?.['name'] ? signInfo?.['name'] + ".git" : (
+                        <>
+                            <span className="text-gray-400 font-bold">_ _</span>
+                            .git
+                        </>
+                    )}
+                </h2>
                 <div className="flex flex-col items-center space-y-[10px] mb-[56px]">
                     <span className="text-gray-400 text-xs font-bold">Mint price</span>
                     <div
@@ -31,6 +59,7 @@ const MintSection = () => {
                     variant={isError ? "destructive" : "default"}
                     className="w-full"
                     onClick={mint}
+                    disabled={!signInfo?.['sig']}
                 >
                     {isLoading ? "Minting..." : "Mint"}
                 </Button>

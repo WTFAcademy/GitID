ab0075e571dd1f80dac90b1b50da95b9077fb06a
b00l <wwgeek@163.com>
2024-05-19 13:28:49 +0800

    chore: 无修改

--- app/(main)/lookup/components/search-input.tsx
+++ app/(main)/lookup/components/search-input.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-function SearchInput() {
+type TProps = {
+  searchValue: string;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+  handleSearch: () => void;
+};
+
+function SearchInput({ setSearchValue, handleSearch, searchValue }: TProps) {
   return (
     <div className="w-full my-12 flex items-center gap-3">
-      <Input placeholder="wtf.git" className="w-full" />
-      <Button>Look up</Button>
+      <Input
+        placeholder="wtf.git"
+        className="w-full"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.name)}
+      />
+      <Button onClick={handleSearch}>Look up</Button>
     </div>
   );
 }
--- app/(main)/lookup/page.tsx
+++ app/(main)/lookup/page.tsx
@@ -1,12 +1,30 @@
+"use client";
+
+import { useState } from "react";
 import SearchInput from "@/app/(main)/lookup/components/search-input";
 import PersonalCard from "@/app/(main)/lookup/components/personal-card";
+import { getPersionalInfo } from "@/lib/api/domain";
+import { TUser } from "@/types";
 
 function Lookup() {
+  const [searchValue, setSearchValue] = useState("smithereens");
+  const [personalInfo, setPersonalInfo] = useState<TUser | null>(null);
+
+  const handleSearch = async () => {
+    const { data } = await getPersionalInfo(searchValue);
+    console.log(data);
+    setPersonalInfo(data);
+  };
+
   return (
     <section className="text-center px-4 sm:px-6 max-w-3xl mx-auto">
       <h1 className="text-5xl mt-28 font-bold">Look up Git.ID</h1>
-      <SearchInput />
-      <PersonalCard />
+      <SearchInput
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        handleSearch={handleSearch}
+      />
+      <PersonalCard personalInfo={personalInfo} />
     </section>
   );
 }

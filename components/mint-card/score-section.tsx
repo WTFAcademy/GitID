38120b23b2e282e3ddc79491dbd8d2c5ea2a5c3d
chongqiangchen <chongqiangchen@gmail.com>
2024-05-19 13:51:14 +0800

    feat: update score

--- components/mint-card/score-section.tsx
+++ components/mint-card/score-section.tsx
@@ -1,4 +1,16 @@
+import {useQuery} from "@tanstack/react-query";
+import {getPersionalInfo} from "@/lib/api/domain";
+import {useAtomValue} from "jotai/index";
+import {githubUserAtom} from "@/lib/store/mint";
+
 const ScoreSection = () => {
+  const githubUser = useAtomValue(githubUserAtom);
+  const {data: user} = useQuery({
+    queryKey: ['getUserInfo', githubUser?.['user_name']],
+    queryFn: () => getPersionalInfo(githubUser?.['user_name']).then(res => res.data),
+  })
+
+  console.log(user);
   return <div />;
 };
 
